bf91508ad2a95ea34f3c4f31f8fdc70ec9b5b9d9
Pierre Bonnafous <pierre.bonnafous@gmail.com>
2022-06-11 16:17:11 +0200

    Add background image, modify login css and add song entity

---
+++ src/Entity/Song.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SongRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SongRepository::class)]
+class Song
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $author;
+
+    #[ORM\Column(type: 'text')]
+    private $text;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $tab;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getTab(): ?string
+    {
+        return $this->tab;
+    }
+
+    public function setTab(?string $tab): self
+    {
+        $this->tab = $tab;
+
+        return $this;
+    }
+}
